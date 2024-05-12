257693b1da00325ea26d060ae0d59fa6cd2a0a88
ujk-droid <03312436713aa@gmail.com>
2024-05-12 11:42:05 -0700

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import react from 'react';
+function Footer(){
+    return(
+        <div className="bg-black
+        text-white
+        h-[30px]
+        flex
+        items-center
+        justify-center">
+            <h1>copy right All Right Reserved!</h1>
+        </div>
+    )
+}
+export default Footer
